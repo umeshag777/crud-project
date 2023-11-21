f470dd5b32cee9f529c930aa5b97cc5a8fe02c0f
Umesha G <umeshag777@gmail.com>
2023-11-21 07:45:48 +0530

    Student Enrollment Commit

---
+++ crud-project/src/main/java/com/example/crudproject/dao/StudentEnrollment.java
@@ -0,0 +1,14 @@
+package com.example.crudproject.dao;
+
+import lombok.Data;
+
+@Data
+public class StudentEnrollment {
+
+    public String firstName;
+
+    public String lastName;
+
+    public Long phoneNumber;
+
+}
